925bb132fedf521409d33e75a223e3ab580c5f6e
Samuel <samudawb@gmail.com>
2022-12-13 11:35:55 +0000

    Petición de datos completada

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -5,6 +5,7 @@ namespace EJE0406
 	{
 		static void Main(string[] args)
 		{
+			int productosRegalados = 0;
 			int cantidadProductos;
 			string[] productos;
 			double[] precios;
@@ -12,7 +13,7 @@ namespace EJE0406
 
 			//Petición de caintidad de productos a comprar
 			Console.WriteLine("Introduzca la cantidad de productos que va a comprar");
-			while (!Int32.TryParse(Console.ReadLine(), out cantidadProductos) || cantidadProductos > 0)
+			while (!Int32.TryParse(Console.ReadLine(), out cantidadProductos) || cantidadProductos <= 0)
 			{
 				Console.WriteLine("Error! La cantidad de productos debe ser un número entero y mayor que 0");
 			}
@@ -21,7 +22,24 @@ namespace EJE0406
 			productos = new string[cantidadProductos];
             precios = new double[cantidadProductos];
 
+			for (int i = 0; i < cantidadProductos; i++)
+			{
+				//Introducir nombre de productos
+				Console.WriteLine("Introduzca el nombre del producto");
+				productos[i] = Console.ReadLine();
 
+				//Introducir precio de producto previamente introducido
+				Console.WriteLine("Introduzca el precio de {0}", productos[i]);
+				while (!Double.TryParse(Console.ReadLine(), out precios[i]) || precios[i] <= 0)
+				{
+					Console.WriteLine("Error! el precio del producto debe ser un número mayor que 0");
+				}
+				//Contador para regalo de productos
+				if (i % 3 == 0)
+				{
+					productosRegalados++;
+				}
+			}
         }
 	}
 }
