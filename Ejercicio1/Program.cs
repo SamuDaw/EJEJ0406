21c066e4d8ae9b2229f72e64ee103d1efbd06a8b
Samuel <samudawb@gmail.com>
2022-12-13 11:52:44 +0000

    Comprocbación número menor

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -6,9 +6,13 @@ namespace EJE0406
 		static void Main(string[] args)
 		{
 			int productosRegalados = 0;
-			int cantidadProductos;
+			int cantidadProductos = 0;
+			int posicionPrecioMenor = 0;
+
 			string[] productos;
 			double[] precios;
+			double[] nuevosPrecios; 
+			double precioMenor = 0;
 			bool salida = false;
 
 			//Petición de caintidad de productos a comprar
@@ -21,25 +25,52 @@ namespace EJE0406
 			//Inicialización de arrays
 			productos = new string[cantidadProductos];
             precios = new double[cantidadProductos];
+            nuevosPrecios = new double[cantidadProductos];
 
-			for (int i = 0; i < cantidadProductos; i++)
+            for (int i = 0; i < cantidadProductos; i++)
 			{
 				//Introducir nombre de productos
 				Console.WriteLine("Introduzca el nombre del producto");
 				productos[i] = Console.ReadLine();
 
-				//Introducir precio de producto previamente introducido
+				//Introducir precio de producto previamente introducido en su vector correspondiente
 				Console.WriteLine("Introduzca el precio de {0}", productos[i]);
 				while (!Double.TryParse(Console.ReadLine(), out precios[i]) || precios[i] <= 0)
 				{
 					Console.WriteLine("Error! el precio del producto debe ser un número mayor que 0");
 				}
+				nuevosPrecios[i] = precios[i];
+
 				//Contador para regalo de productos
 				if (i % 3 == 0)
 				{
 					productosRegalados++;
 				}
+				//Comprobación de precio menor
+				if (i == 0)
+				{
+					precioMenor = precios[i];
+				}
+				else
+				{
+					if (precios[i] < precioMenor)
+					{
+						precioMenor = precios[i];
+						posicionPrecioMenor = i;
+					}
+				}
 			}
+			//Comprobación de 
+			if (productosRegalados > 0)
+			{
+				nuevosPrecios[posicionPrecioMenor] = 0;
+
+                for (int i = 0; i < cantidadProductos; i++)
+                {
+                    nuevosPrecios
+			    }
+            }
+
         }
 	}
 }
